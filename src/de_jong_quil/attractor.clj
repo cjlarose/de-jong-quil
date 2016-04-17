014539bfbebdc61ea9d8661044379305bd8c5556
Chris LaRose <cjlarose@gmail.com>
2016-04-17 00:14:35 -0700

    Render a grid of differently sized circles corresponding with probability

--- src/de_jong_quil/attractor.clj
+++ src/de_jong_quil/attractor.clj
@@ -1,7 +1,8 @@
 (ns de-jong-quil.attractor
   (:require [quil.core :as q]))
 
-(def num-points (bit-shift-left 1 20))
+(def num-points 250)
+(def GRID_SIZE 50)
 
 (defn random-points []
   (repeatedly (fn [] [(- (rand 4.0) 2.0)
@@ -12,28 +13,31 @@
     [(- (Math/sin (* a y)) (Math/cos (* b x)))
      (- (Math/sin (* c x)) (Math/cos (* d y)))]))
 
+(defn probability-grid [points]
+  (->> points
+       (map (fn [[x y]] [(Math/round (* (+ x 2.0) (/ GRID_SIZE 4.0)))
+                         (Math/round (* (+ y 2.0) (/ GRID_SIZE 4.0)))]))
+       (frequencies)
+       (reduce (fn [memo [k v]] (assoc memo k (/ (double v) (count points)))) {})))
+
 (defn setup []
   (q/frame-rate 1)
   (q/color-mode :hsb)
-  ; (q/no-stroke)
+  (q/no-stroke)
   (let [f (de-jong 3.14 3.14 3.14 3.14)
         points (map (comp f f f f f) (take num-points (random-points)))]
     {:color 0
      :points points}))
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
   (q/background 255)
-  (q/stroke 0 255 255 60)
+  (q/fill 0 255 255 60)
 
-  ; (q/camera (/ (q/width) 2) (/ (q/height) 2) 40 (/ (q/width) 2) (/ (q/height) 2) 0 0 1 0)
-
-  (let [w-scale (/ (q/width) 4)
-        h-scale (/ (q/height) 4)]
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      (q/begin-shape :points)
-      (doseq [[idx [x y]] (map-indexed vector (:points state))]
-        (q/vertex (* x w-scale 0.8) (* y h-scale 0.8)))
-      (q/end-shape)))
-  (q/exit))
+  (let [dx (/ (q/width) GRID_SIZE)
+        dy (/ (q/height) GRID_SIZE)
+        probs (probability-grid (:points state))]
+    (doseq [col (range GRID_SIZE)
+            row (range GRID_SIZE)]
+      (let [prob (get probs [col row] 0.0)
+            size (* (Math/pow prob 0.5) 100)]
+        (q/ellipse (+ (* col dx) (/ dx 2)) (+ (* row dy) (/ dy 2)) size size)))))
--- src/de_jong_quil/core.clj
+++ src/de_jong_quil/core.clj
@@ -6,14 +6,14 @@
 (defn -main []
   (q/defsketch de-jong-quil
     :title "You spin my circle right round"
-    :size [2400 2400]
+    :size [1000 1000]
     ; setup function called only once, during sketch initialization.
     :setup setup
     ; update-state is called on each iteration before draw-state.
     :draw draw-state
 
-    :renderer :pdf
-    :output-file "export.pdf"
+    ; :renderer :pdf
+    ; :output-file "export.pdf"
 
     ; This sketch uses functional-mode middleware.
     ; Check quil wiki for more info about middlewares and particularly
